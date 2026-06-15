2a9dc53110132c45b100f05ad895741b68cc9cd3
Arunjot-ATR592 <arunjot@antiersolutions.com>
2026-06-15 22:55:18 +0530

    chore: Added first program

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+// Online Rust compiler to run Rust program online
+// Print "Start small. Ship something." message
+
+fn main() {
+  let star = "*";
+  for _ in 0..5 {
+    for _ in 1..5{
+         print!("{}",star);
+    }
+     println!("{}",star);
+ }
+}
